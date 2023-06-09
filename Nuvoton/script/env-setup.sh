3892b63b509b2e21858310e485dd3409549d9b1f
Chih-Chiang Chang <ccchang12@nuvoton.com>
2023-06-09 15:48:36 +0800

    MA35D1 & NUC980: Support configure in environment setup
    Change-Id: Iabef50157a6a5725608ee2fd01265d90d27d6ce4

--- Nuvoton/script/env-setup.sh
+++ Nuvoton/script/env-setup.sh
@@ -19,25 +19,34 @@ echo "* Kernel compilation variables (ARCH, CROSS_COMPILE, KERNELDIR) are export
 echo "" >> ${ENV_SETUP_FILE}
 echo "EOF" >> ${ENV_SETUP_FILE}
 
-echo "export \"AR="${ARCH}"-openwrt-linux-ar"\" >> ${ENV_SETUP_FILE}
-echo "export \"AS="${ARCH}"-openwrt-linux-as"\" >> ${ENV_SETUP_FILE}
-echo "export \"LD="${ARCH}"-openwrt-linux-ld"\" >> ${ENV_SETUP_FILE}
-echo "export \"NM="${ARCH}"-openwrt-linux-nm"\" >> ${ENV_SETUP_FILE}
-echo "export \"CC="${ARCH}"-openwrt-linux-gcc"\" >> ${ENV_SETUP_FILE}
-echo "export \"GCC="${ARCH}"-openwrt-linux-gcc"\" >> ${ENV_SETUP_FILE}
-echo "export \"CPP="${ARCH}"-openwrt-linux-cpp"\" >> ${ENV_SETUP_FILE}
-echo "export \"CXX="${ARCH}"-openwrt-linux-g++"\" >> ${ENV_SETUP_FILE}
-echo "export \"RANLIB="${ARCH}"-openwrt-linux-ranlib"\" >> ${ENV_SETUP_FILE}
-echo "export \"READELF="${ARCH}"-openwrt-linux-readelf"\" >> ${ENV_SETUP_FILE}
-echo "export \"STRIP="${ARCH}"-openwrt-linux-strip"\" >> ${ENV_SETUP_FILE}
-echo "export \"OBJCOPY="${ARCH}"-openwrt-linux-objcopy"\" >> ${ENV_SETUP_FILE}
-echo "export \"OBJDUMP="${ARCH}"-openwrt-linux-objdump"\" >> ${ENV_SETUP_FILE}
+echo "export \"AR="${GNU_TARGET_NAME}"-ar"\" >> ${ENV_SETUP_FILE}
+echo "export \"AS="${GNU_TARGET_NAME}"-as"\" >> ${ENV_SETUP_FILE}
+echo "export \"LD="${GNU_TARGET_NAME}"-ld"\" >> ${ENV_SETUP_FILE}
+echo "export \"NM="${GNU_TARGET_NAME}"-nm"\" >> ${ENV_SETUP_FILE}
+echo "export \"CC="${GNU_TARGET_NAME}"-gcc"\" >> ${ENV_SETUP_FILE}
+echo "export \"GCC="${GNU_TARGET_NAME}"-gcc"\" >> ${ENV_SETUP_FILE}
+echo "export \"CPP="${GNU_TARGET_NAME}"-cpp"\" >> ${ENV_SETUP_FILE}
+echo "export \"CXX="${GNU_TARGET_NAME}"-g++"\" >> ${ENV_SETUP_FILE}
+echo "export \"RANLIB="${GNU_TARGET_NAME}"-ranlib"\" >> ${ENV_SETUP_FILE}
+echo "export \"READELF="${GNU_TARGET_NAME}"-readelf"\" >> ${ENV_SETUP_FILE}
+echo "export \"STRIP="${GNU_TARGET_NAME}"-strip"\" >> ${ENV_SETUP_FILE}
+echo "export \"OBJCOPY="${GNU_TARGET_NAME}"-objcopy"\" >> ${ENV_SETUP_FILE}
+echo "export \"OBJDUMP="${GNU_TARGET_NAME}"-objdump"\" >> ${ENV_SETUP_FILE}
 echo "export \"CPPFLAGS="${TARGET_CPPFLAGS}\" >> ${ENV_SETUP_FILE}
 echo "export \"CFLAGS="${TARGET_CFLAGS}\" >> ${ENV_SETUP_FILE}
 echo "export \"CXXFLAGS="${TARGET_CXXFLAGS}\" >> ${ENV_SETUP_FILE}
 echo "export \"STAGING_DIR="${STAGING_DIR}\" >> ${ENV_SETUP_FILE}
 echo "export \"ARCH="${LINUX_KARCH}\" >> ${ENV_SETUP_FILE}
-echo "export \"CROSS_COMPILE="${ARCH}"-openwrt-linux-"\" >> ${ENV_SETUP_FILE}
+echo "export \"CROSS_COMPILE="${GNU_TARGET_NAME}"-"\" >> ${ENV_SETUP_FILE}
+echo "export \"CONFIGURE_FLAGS=--target="${GNU_TARGET_NAME}" \
+--host="${GNU_TARGET_NAME}" \
+--build="${GNU_HOST_NAME}" \
+--prefix="${TOOLCHAIN_DIR}"/usr \
+--exec-prefix="${TOOLCHAIN_DIR}"/usr \
+--sysconfdir="${TOOLCHAIN_DIR}"/etc \
+--localstatedir="${TOOLCHAIN_DIR}"/var \
+--program-prefix=\"" >> ${ENV_SETUP_FILE}
+echo "alias configure=\"./configure \${CONFIGURE_FLAGS}\"" >> ${ENV_SETUP_FILE}
 echo "export \"PATH="${TOOLCHAIN_DIR}/bin:\$PATH\" >> ${ENV_SETUP_FILE}
 echo "export \"KERNELDIR="${LINUX_DIR}\" >> ${ENV_SETUP_FILE}
 
