1ce2c4b085eb93815cf232e20cb5bd2870c64544
Chih-Chiang Chang <ccchang12@nuvoton.com>
2024-06-06 10:32:36 +0800

    MA35: : Support emWin package
    Change-Id: Id2544e957e8c7c46c7bd443a32aec50f306a2ca0

--- package/nuvoton/tslib/files/etc/profile.d/tslib.sh
+++ package/nuvoton/tslib/files/etc/profile.d/tslib.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
-if [ -e /dev/input/event1 ]; then
+ARCH=$(uname -m)
+if [ "$ARCH" = "aarch64" ]; then
+	TSLIB_TSDEVICE=/dev/input/event0
+elif [ -e /dev/input/event1 ]; then
 	TSLIB_TSDEVICE=/dev/input/event1
-elif [ -e /dev/input/event0 ]; then
+else
 	TSLIB_TSDEVICE=/dev/input/event0
 fi
-	TSLIB_CONFFILE=/etc/ts.conf
-	TSLIB_CALIBFILE=/etc/pointercal
-	LD_LIBRARY_PATH=/lib/ts/
+TSLIB_CONFFILE=/etc/ts.conf
+TSLIB_CALIBFILE=/etc/pointercal
+LD_LIBRARY_PATH=/lib/ts/
 
-	export TSLIB_TSDEVICE
-	export TSLIB_CONFFILE
-	export TSLIB_CALIBFILE
-	export LD_LIBRARY_PATH
+export TSLIB_TSDEVICE
+export TSLIB_CONFFILE
+export TSLIB_CALIBFILE
+export LD_LIBRARY_PATH
